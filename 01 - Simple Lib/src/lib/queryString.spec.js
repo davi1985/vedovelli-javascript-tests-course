d7bfa4fbd479d8dea803971de7f67e2e155df16c
Davi Silva <davisilvaphoto@gmail.com>
2022-04-03 11:23:49 -0300

    add methods queryString parse

--- 01 - Simple Lib/src/lib/queryString.spec.js
+++ 01 - Simple Lib/src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -8,5 +8,51 @@ describe('Object to query string', () => {
     };
 
     expect(queryString(obj)).toBe('name=Davi&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passed as value ', () => {
+    const obj = {
+      name: 'Davi',
+      skills: ['JS', 'React', 'Typescript'],
+    };
+
+    expect(queryString(obj)).toBe('name=Davi&skills=JS,React,Typescript');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Davi',
+      skills: { first: 'JS', second: 'React' },
+    };
+
+    expect(() => queryString(obj)).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Davi&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Davi',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string of a single key-value to object', () => {
+    const qs = 'name=Davi';
+
+    expect(parse(qs)).toEqual({
+      name: 'Davi',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated valuess', () => {
+    const qs = 'name=Davi&skills=JS,React';
+
+    expect(parse(qs)).toEqual({
+      name: 'Davi',
+      skills: ['JS', 'React'],
+    });
   });
 });
